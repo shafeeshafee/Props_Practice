83fb11be1088cde7dc5ca5e0280408a052385eaa
Shafee <shafeelinks@gmail.com>
2021-01-06 20:59:16 -0500

    book data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Components/Header";
 import Book from "./Components/Book";
 import Footer from "./Components/Footer";
+import Data from "./Data";
 
 function App() {
 	return (
@@ -9,29 +10,11 @@ function App() {
 			<header className="App-header">
 				<Header />
 			</header>
-			{/* Book 1 */}
-			<Book
-				title="Love in the Time of Cholera"
-				description="A story about unrequited love. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				author="Gabriel Garcia Marquez"
-				image="https://images-na.ssl-images-amazon.com/images/I/41+HorgT4YL._SX322_BO1,204,203,200_.jpg"
-			/>
-			{/* Book 2 */}
-			<Book
-				title="The Stranger"
-				description="L'Étranger is a 1942 novel by French author Albert Camus. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-				author="Albert Camus"
-				image="https://images-na.ssl-images-amazon.com/images/I/517eqjeVDzL._SX322_BO1,204,203,200_.jpg"
-			/>
+			<div>
+				{Data.map((book) => {
+					return <Book title={book.title} image={book.image} author={book.author} description={book.description} />;
+				})}
+			</div>
 			<Footer />
 		</div>
 	);
---
+++ src/Data.js
@@ -0,0 +1,48 @@
+const Data = [
+	{
+		title: "Love in the Time of Cholera",
+		description: `A story about unrequited love. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		author: "Gabriel Garcia Marquez",
+		image: "https://images-na.ssl-images-amazon.com/images/I/41+HorgT4YL._SX322_BO1,204,203,200_.jpg",
+	},
+	{
+		title: "The Stranger",
+		description: `A story about unrequited love. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		author: "Camus",
+		image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590930002i/49552._UY2400_SS2400_.jpg",
+	},
+	{
+		title: "100 Years of Solitude",
+		description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		author: "Gabriel Garcia Marquez",
+		image: "https://images-na.ssl-images-amazon.com/images/I/91giPifN6fL._AC_UL600_SR396,600_.jpg",
+	},
+	{
+		title: "The Brothers Karamazov",
+		description: `A story about unrequited love. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+		author: "Fyodor Dostoevsky",
+		image: "https://images-na.ssl-images-amazon.com/images/I/8117HB7WbvL.jpg",
+	},
+];
+
+export default Data;
